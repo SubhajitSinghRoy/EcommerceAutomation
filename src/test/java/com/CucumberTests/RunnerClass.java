cba28bfb2f090a601f0ad49a682e574c093511c6
Subhajit Singh Roy <subhojits74@gmail.com>
2023-05-27 10:16:02 +0530

    removed unused imports

--- src/test/java/com/CucumberTests/RunnerClass.java
+++ src/test/java/com/CucumberTests/RunnerClass.java
@@ -2,8 +2,6 @@ package com.CucumberTests;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import jdk.management.resource.internal.ResourceNatives;
-import org.junit.runner.RunWith;
 
 @CucumberOptions(features = "src/test/java/com/CucumberTests/SubmitOrder.feature",
         glue = {"stepDefinition"},
